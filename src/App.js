41f686205536ed23f22057c0fa1f0ca85d1bc80e
Yarden <yarden.paniry@gmail.com>
2022-03-29 18:48:57 +0300

    commit message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PTtable from './components/PTtable';
+
+function App() {
+  return (
+    <div className="App">
+     <PTtable/>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/PTtable.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { MDBDataTable } from 'mdbreact';
+import theHarvesterOutput from '../theHarvesterOutput.json'
+
+
+const PTtable = () => {
+  const rows = theHarvesterOutput.map(item => ({ asset: item.asset, IPaddress: item.IP_Address }))
+  const data = {
+    columns: [
+      {
+        label: 'asset',
+        field: 'asset',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'IP address',
+        field: 'IPaddress',
+        sort: 'asc',
+        width: 270
+      }
+    ],
+    rows
+ 
+  };
+
+  return (
+    <MDBDataTable
+   //  entriesOptions={false} 
+     entries={10} 
+      searching={false}
+    //   striped
+    //   bordered
+    //   small
+    hover
+      data={data}
+    />
+  );
+}
+
+export default PTtable;
